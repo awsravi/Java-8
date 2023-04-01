3e4fc49ffe9575d4fad36ee76d833395b09280be
Ravikumar <127386201+awsravi@users.noreply.github.com>
2023-04-01 15:27:32 +0530

    Update and rename FindMinimumnumberStream.java to FindMinMaxNumStream.java

--- FindMinMaxNumStream.java
+++ FindMinMaxNumStream.java
@@ -3,7 +3,7 @@ package com.java8.quescol;
 import java.util.Comparator;
 import java.util.stream.Stream;
 
-public class FindMinimumnumberStream {
+public class FindMinMaxNumStream {
 public static void main(String[] args) {
  	 Integer min=Stream.of(1,2,3,4,5,6).min(Comparator.comparing(Integer::valueOf)).get();
 	System.out.println(min);
